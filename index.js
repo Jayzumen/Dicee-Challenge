d96fd231c0aab440b34ceb8c5d64f2f71b9e261c
jayzumen <reinhardtjanniklas96@gmail.com>
2022-06-10 15:07:23 +0200

    cleaned code and centered the page

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function diceGame(){
+    // randomized Dice 1
     var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
 
     var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
@@ -10,6 +11,8 @@ function diceGame(){
     image1.setAttribute("src", randomImageSource);
 
 
+
+    // randomized Dice 2
     var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
     var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
@@ -17,6 +20,7 @@ function diceGame(){
     document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 
+    // declare the winner
     if (randomNumber1 > randomNumber2) {
     document.querySelector("h1").innerHTML = "Player 1 Wins!";
     }
@@ -28,39 +32,5 @@ function diceGame(){
     }
 }
 
-
+// enable the button to work
 document.querySelector("button").setAttribute("onclick", "diceGame()");
-
-// // randomized Dice 1
-
-// var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
-
-// var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
-
-// var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
-
-// var image1 = document.querySelectorAll("img")[0];
-
-// image1.setAttribute("src", randomImageSource);
-
-// // randomized Dice 2
-
-// var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-
-// var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
-
-// document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
-
-
-// // declare the winner
-
-// if (randomNumber1 > randomNumber2) {
-//   document.querySelector("h1").innerHTML = "Play 1 Wins!";
-// }
-// else if (randomNumber2 > randomNumber1) {
-//   document.querySelector("h1").innerHTML = "Player 2 Wins!";
-// }
-// else {
-//   document.querySelector("h1").innerHTML = "Draw!";
-// }
-
